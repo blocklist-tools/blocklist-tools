dd85512785a660621de5843d623632f1679d8cc5
Daniel <daniel@developerdan.com>
2020-08-13 20:48:41 +0000

    Added a couple more plugins, fixed broken settings.xml

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -e
 
+if [[ ! -f '/root/.m2/settings.xml' ]]; then
+    cp '/opt/blocklist-tools/settings.xml' '/root/.m2/settings.xml'
+fi
+
 echo "${GPG_PASSPHRASE}" | gpg --import /run/secrets/gpg-key.asc
-echo "43CB23B0D79C3B8E92E610174940B41048AF73EA:6" | gpg --import-ownertrust
+echo '43CB23B0D79C3B8E92E610174940B41048AF73EA:6' | gpg --import-ownertrust
 
 /usr/local/bin/mvn-entrypoint.sh mvn "$@"
