3f66055752d59033389012fe102b9ff6be220f9a
Daniel <daniel@developerdan.com>
2020-08-14 03:30:35 +0000

    Install git

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,6 +5,13 @@ if [[ ! -f '/root/.m2/settings.xml' ]]; then
     cp '/opt/blocklist-tools/settings.xml' '/root/.m2/settings.xml'
 fi
 
+if [[ "$@" =~ 'deploy' ]]
+then
+   yum install git --quiet --assumeyes
+   git config --global user.email 'daniel@developerdan.com'
+   git config --global user.name 'Daniel'
+fi
+
 echo "${GPG_PASSPHRASE}" | gpg --import /run/secrets/gpg-key.asc
 echo '43CB23B0D79C3B8E92E610174940B41048AF73EA:6' | gpg --import-ownertrust
 
