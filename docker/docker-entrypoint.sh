c1811925a047dc0432d0e17a7fa7be985871cb87
Daniel <daniel@developerdan.com>
2020-09-01 21:24:06 +0000

    Fixed broken build due to maven docker chagnes

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 set -e
 
 if [[ ! -f '/root/.m2/settings.xml' ]]; then
@@ -7,12 +8,20 @@ fi
 
 if [[ "$@" =~ 'deploy' ]]
 then
-   yum install git --quiet --assumeyes
-   git config --global user.email 'daniel@developerdan.com'
-   git config --global user.name 'Daniel'
-fi
+    apt-get update
+    apt-get install git gnupg2 -y
+    git config --global user.email 'daniel@developerdan.com'
+    git config --global user.name 'Daniel'
 
-echo "${GPG_PASSPHRASE}" | gpg --import /run/secrets/gpg-key.asc
-echo '43CB23B0D79C3B8E92E610174940B41048AF73EA:6' | gpg --import-ownertrust
+    mkdir -p ~/.gnupg
+    chmod 700 ~/.gnupg
+    echo "use-agent" >> ~/.gnupg/gpg.conf
+    echo "pinentry-mode loopback"  >> ~/.gnupg/gpg.conf
+    echo "allow-loopback-pinentry" >> ~/.gnupg/gpg-agent.conf
+    echo RELOADAGENT | gpg-connect-agent
+
+    echo "${GPG_PASSPHRASE}" | gpg --import /run/secrets/gpg-key.asc
+    echo '43CB23B0D79C3B8E92E610174940B41048AF73EA:6' | gpg --import-ownertrust
+fi
 
 /usr/local/bin/mvn-entrypoint.sh mvn "$@"
