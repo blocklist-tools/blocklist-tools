e5e65a9459b9c1644b95d8ce5b2e5beabbae3eb6
Daniel <daniel@developerdan.com>
2020-11-05 21:44:25 +0000

    Bump versions - speed up ListDiff

--- src/main/java/com/developerdan/blocklist/tools/ListDiff.java
+++ src/main/java/com/developerdan/blocklist/tools/ListDiff.java
@@ -6,25 +6,18 @@ import java.util.TreeSet;
 public class ListDiff {
     public static String DomainList(final NavigableSet<Domain> before, final NavigableSet<Domain> after) {
         var patchBuilder = new PatchBuilder();
-        TreeSet<Domain> removedDomains = ListDiff.difference(before, after);
-        TreeSet<Domain> addedDomains = ListDiff.difference(after, before);
+
         TreeSet<Domain> allDomains = new TreeSet<>(before);
         allDomains.addAll(after);
         for(Domain entry : allDomains) {
-            if (removedDomains.contains(entry)) {
-                patchBuilder.removed(entry);
-            } else if (addedDomains.contains(entry)) {
+            if (!before.contains(entry)) {
                 patchBuilder.added(entry);
+            } else if (!after.contains(entry)) {
+                patchBuilder.removed(entry);
             } else {
                 patchBuilder.noChange(entry);
             }
         }
         return patchBuilder.toString();
-    }
-
-    private static TreeSet<Domain> difference(final NavigableSet<Domain> before, final NavigableSet<Domain> after) {
-        TreeSet<Domain> removedDomains = new TreeSet<>(before);
-        removedDomains.removeAll(after);
-        return removedDomains;
     }
 }
